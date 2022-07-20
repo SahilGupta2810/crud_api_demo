04f6c8743e7ea700e90d39e723a18665a200ec0a
Sahil Gupta <guptasahil2810@gmail.com>
2022-07-20 22:17:44 +0530

    Initial commit

---
+++ test/crud_api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrudApi from '../lib/crud_api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/crud_api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrudApi.CrudApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
